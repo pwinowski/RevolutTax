5e0fecebbaeaf79b2ac0504d7b11a2f6ddf23678
Patryk Winowski <patryk.winowski@s3connectedhealth.com>
2024-02-11 00:12:03 +0100

    add complete doc for getExchangeRates

--- modules/nbp/nbp.py
+++ modules/nbp/nbp.py
@@ -3,7 +3,25 @@ import requests
 baseURL = "https://api.nbp.pl/api/"
 
 def getExchangeRates(year, currency='usd'):
-    """Fetches exchange rates from NBP API"""
+    """
+    Fetches exchange rates from the NBP API for a specific year and currency.
+
+    Args:
+        year (int): The year for which to fetch exchange rates.
+        currency (str, optional): The currency code for which to fetch exchange rates. Defaults to 'usd'.
+
+    Returns:
+        dict: A dictionary mapping dates to exchange rate values for the specified year and currency.
+            The keys are strings representing dates in the format 'YYYY-MM-DD', and the values are floats.
+
+    Raises:
+        requests.HTTPError: If the request to the NBP API fails with an HTTP error status code.
+        Exception: If any other exception occurs during the request or processing of the data.
+
+    Example:
+        >>> nbp.getExchangeRates(2023, 'eur')
+        {'2023-01-01':  4.50, '2023-01-02':  4.55, ...}
+    """
     endpoint = f"exchangerates/rates/a/{currency}/"
     try:
         startDate = f"{year}-01-01"
