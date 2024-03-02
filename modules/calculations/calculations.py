11c021d2b411eabba5ceb6b91486747709e2df32
Patryk Winowski <patryk.winowski@s3connectedhealth.com>
2024-03-02 12:07:52 +0100

    add pnl_per_year and tests

--- modules/calculations/calculations.py
+++ modules/calculations/calculations.py
@@ -22,3 +22,26 @@ def usd_to_pln(transactions: Dict[datetime, Decimal], rates: Dict[datetime, Deci
         pln_transactions[date] = pln_amount
         
     return pln_transactions
+
+
+def pnl_per_year(transactions: Dict[datetime, Decimal]) -> Dict[int, Decimal]:
+    """
+    Calculates the PnL per year for each year in the provided transactions.
+
+    Args:
+        transactions (Dict[datetime, Decimal]): A dictionary containing transactions.
+
+    Returns:
+        Dict[int, Decimal]: A dictionary containing the PnL per year for each year in the provided transactions.
+    """
+
+    pnl_per_year = {}
+
+    for date, amount in transactions.items():
+        year = date.year
+        if year not in pnl_per_year:
+            pnl_per_year[year] = amount
+        else:
+            pnl_per_year[year] += amount
+
+    return pnl_per_year
--- modules/calculations/calculations_tests.py
+++ modules/calculations/calculations_tests.py
@@ -3,9 +3,7 @@ from datetime import datetime
 from decimal import Decimal
 import calculations as calc
 
-
-
-class TestUsdToPln(unittest.TestCase):
+class CalculationTests(unittest.TestCase):
 
     def setUp(self):
         # Sample transactions in USD
@@ -27,6 +25,40 @@ class TestUsdToPln(unittest.TestCase):
     def test_usd_to_pln(self):
         result = calc.usd_to_pln(self.transactions, self.rates)
         self.assertEqual(result, self.expected_pln_transactions)
+
+    def test_pnl_per_year(self):
+        # Sample transactions
+        transactions = {
+            datetime(2023, 1, 1): Decimal('100.00'),
+            datetime(2023, 1, 2): Decimal('200.00'),
+            datetime(2024, 1, 1): Decimal('300.00'),
+            datetime(2024, 1, 2): Decimal('400.00'),
+        }
+        # Expected PnL per year
+        expected_pnl_per_year = {
+            2023: Decimal('300.00'),
+            2024: Decimal('700.00'),
+        }
+    
+        result = calc.pnl_per_year(transactions)
+        self.assertEqual(result, expected_pnl_per_year)
+
+    def test_pnl_per_year_with_losses(self):
+        # Sample transactions with losses
+        transactions = {
+            datetime(2023, 1, 1): Decimal('100.00'),
+            datetime(2023, 1, 2): Decimal('-200.00'), # Loss
+            datetime(2024, 1, 1): Decimal('300.00'),
+            datetime(2024, 1, 2): Decimal('-400.00'), # Loss
+        }
+        # Expected PnL per year
+        expected_pnl_per_year = {
+            2023: Decimal('-100.00'), # Loss
+            2024: Decimal('-100.00'), # Loss
+        }
+    
+        result = calc.pnl_per_year(transactions)
+        self.assertEqual(result, expected_pnl_per_year)
 
 if __name__ == '__main__':
     unittest.main()
